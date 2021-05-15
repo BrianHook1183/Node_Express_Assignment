cf9c9d40cef5dff006c7eb2b2294082dbcc345e9
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-05-15 01:03:26 -0500

    initial route structure

--- src/app.js
+++ src/app.js
@@ -4,7 +4,21 @@ const app = express();
 const getZoos = require("./utils/getZoos");
 const validateZip = require("./middleware/validateZip");
 
+// Routes
 
+app.get("/check/:zip", (req, res, next) => {
+  const zip = req.params.zip;
+  res.send(`check/${zip}`);
+});
+
+app.get("/zoos/all", (req, res, next) => {
+  res.send("zoos/all");
+});
+
+app.get("/zoos/:zip", (req, res, next) => {
+  const zip = req.params.zip;
+  res.send(`zoos/${zip}`);
+});
 
 app.use(morgan("dev"));
 
